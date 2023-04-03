9670e45ee6d17166bcae15c45d2e7690622177e0
Ankush36850 <joshiankush36850@gmail.com>
2023-04-03 12:55:43 +0530

    Roll The Dice

---
+++ app.js
@@ -0,0 +1,29 @@
+let btns = document.querySelectorAll('button')
+let output = document.querySelector('.result')
+let players = {}
+
+for(let t of btns){
+    t.addEventListener("click", rollDice)
+}
+
+let arr = []
+let str = ''
+function rollDice(event){
+    this.disabled = true;
+    if(event.target.innerText == 'End Button'){
+        arr.sort(function(a,b){return b - a})
+        for(let t in players){
+            if(players[t] == arr[0]){
+                str = str + t + ','
+            }
+        }
+        output.innerText = `Winners are ${str}`        
+    }
+    else{
+        let result = parseInt((Math.random()*6)+1) 
+        event.target.nextElementSibling.innerText = `Value you got : ${result}`
+        arr.push(result)
+        let playerName = document.querySelector('#'+ event.srcElement.className).value
+        players[playerName] = result
+    }
+}
