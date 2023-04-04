c06376b4c838d9788a3a670587dca2e4d5500142
Ankush36850 <joshiankush36850@gmail.com>
2023-04-04 10:16:26 +0530

    "Changes to app.js"

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ for(let t of btns){
     t.addEventListener("click", rollDice)
 }
 
+let result = 0
 let arr = []
 let str = ''
 function rollDice(event){
     this.disabled = true;
     if(event.target.innerText != 'End Button'){
-        let result = parseInt((Math.random()*6)+1) 
+        result = parseInt((Math.random()*6)+1) 
         event.target.nextElementSibling.innerText = `Value you got : ${result}`
         arr.push(result)
         let playerName = document.querySelector('#'+ event.srcElement.className).value
@@ -20,11 +21,18 @@ function rollDice(event){
     }
     else{
         arr.sort(function(a,b){return b - a})
+        let count = 0
         for(let t in players){
             if(players[t] == arr[0]){
                 str = str + t + ','
+                count++
             }
         }
-        output.innerText = `Winners are ${str}`     
+        if(count > 1){
+            output.innerText = `Winners are ${str} as they score ${arr[0]}`
+        }
+        else{
+            output.innerText = `Winner is ${str} as they score ${arr[0]}`
+        }             
     }
 }
